03f5d11595613cbba4bc3242f63c2532e5ae7b46
HARSHITH C MAHESH <harru2411040@gmail.com>
2016-06-28 18:16:59 -0500

    Modified MainActivity. Moved instance variable button into method Oncreate.

--- app/src/main/java/team7/uta/com/findfriends/MainActivity.java
+++ app/src/main/java/team7/uta/com/findfriends/MainActivity.java
@@ -10,14 +10,14 @@ import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        login = (Button) findViewById(R.id.register);
+        Button login = (Button) findViewById(R.id.register);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
