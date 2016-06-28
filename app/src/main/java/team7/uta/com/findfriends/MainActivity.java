7663e2ba00efd6f4559532c0d4a7b28f93417c8d
HARSHITH C MAHESH <harru2411040@gmail.com>
2016-06-28 18:05:23 -0500

    Initial commit

---
+++ app/src/main/java/team7/uta/com/findfriends/MainActivity.java
@@ -0,0 +1,30 @@
+package team7.uta.com.findfriends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        login = (Button) findViewById(R.id.register);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register_intent = new Intent(MainActivity.this, register.class);
+                startActivity(register_intent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/team7/uta/com/findfriends/register.java
@@ -0,0 +1,15 @@
+package team7.uta.com.findfriends;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by Harshith on 6/28/2016.
+ */
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
