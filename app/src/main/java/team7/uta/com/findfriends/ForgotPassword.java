0ba30514d41fc40e8edf873aabf4b3e858229411
aishwaryarajan75 <aishwaryarajan75@gmail.com>
2016-07-03 21:23:31 -0500

    redirection to login

--- app/src/main/java/team7/uta/com/findfriends/ForgotPassword.java
+++ app/src/main/java/team7/uta/com/findfriends/ForgotPassword.java
@@ -2,12 +2,36 @@ package team7.uta.com.findfriends;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.StrictMode;
 
+import org.apache.http.client.methods.HttpPost;
+
+import java.sql.ResultSet;
+import android.util.Log;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
 public class ForgotPassword extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+    }
+
+    public void doLogin(View view) {
+        Intent intentl = new Intent(ForgotPassword.this, MainActivity.class);
+        startActivity(intentl);
     }
 }
